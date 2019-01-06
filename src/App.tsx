afe1ab1e36fca6acea87188ad332cde9b444dbfa
Aleksey Dmitriev <ad@blockchaininstitute.io>
2019-01-06 16:10:13 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
 
-class App extends Component {
-  render() {
+export class App extends PureComponent {
+  render (): React.ReactNode {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='rt-app'>
+      
       </div>
     );
   }
 }
-
-export default App;
